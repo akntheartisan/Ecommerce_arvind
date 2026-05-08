35e3c2674eb9a37a8b4db2f9c0d65c82423a6d83
akntheartisan <arvindnagaraj.n@gmail.com>
2026-05-08 15:34:59 +0530

    server run error fixed

--- server/index.js
+++ server/index.js
@@ -6,63 +6,57 @@ const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const { connection } = require("mongoose");
 
-//importing configs and middlewares
 const configCloudinary = require("./Config/Cloudinary");
 const {
   appEnvs: { port },
 } = require("./Config/env");
 const connectDB = require("./Config/DB");
-
 const errorHandler = require("./Middlewares/errorHandling");
 
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://ecommerce-arvind-client.vercel.app"
+  "https://ecommerce-arvind-client.vercel.app",
 ];
 
 app.use(
   cors({
     origin: function (origin, cb) {
       if (!origin) return cb(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return cb(null, true);
-      }
-
+      if (allowedOrigins.includes(origin)) return cb(null, true);
       return cb(new Error("Blocked by CORS"));
     },
     methods: ["GET", "POST", "DELETE", "PATCH"],
     credentials: true,
-  }),
-)
+  })
+);
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser());
 
-//importing routes
 const userRoute = require("./Route/userRoute");
 const productRoute = require("./Route/Admin/productRoute");
 
-//connecting DB
 connectDB();
+configCloudinary(); 
 
-//routes
 app.get("/", (req, res) => {
   res.send("Hello from ecommerce api!");
 });
 
-app.use("/user",userRoute)
+app.use("/user", userRoute);
 app.use("/product", productRoute);
-
-//error middlewares
 app.use(errorHandler);
 
-//running server
-connection.on("connected", () => {
-  app.listen(port, () => {
-    configCloudinary();
-    console.log(`Server is running on port ${port}`);
+
+if (process.env.NODE_ENV !== "production") {
+  connection.on("connected", () => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   });
-});
+}
+
+
+module.exports = app;
